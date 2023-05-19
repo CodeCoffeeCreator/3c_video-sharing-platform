33311d58b775f889677c5381dfd96496024dfef9
Denny Pearce <code.coffee.creator@gmail.com>
2023-05-19 14:19:21 +0300

    final layout, but not end

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 (function (jQuery) {
   'use strict';
   jQuery(document).ready(function () {
+    function activaTav(pill) {
+      jQuery(pill).addClass('active show');
+    }
+
     // sticky header anmation and height
     function headerHeight() {
       var height = jQuery('#main-header').height();
@@ -201,5 +205,86 @@
         },
       ],
     });
+
+    jQuery('#trending-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      draggable: false,
+      asNavFor: '#trending-slider-nav',
+    });
+
+    jQuery('#trending-slider-nav').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      asNavFor: '#trending-slider',
+      dots: false,
+      arrows: true,
+      nextArrow:
+        '<a href="#" class="slick-arrow slick-next"><i class="fa fa-chevron-right"></i></a>',
+      prevArrow:
+        '<a href="#" class="slick-arrow slick-prev"><i class="fa fa-chevron-left"></i></a>',
+      infinite: true,
+      centerMode: true,
+      centerPadding: 0,
+      focusOnSelect: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
+
+    jQuery('.episodes-slider1').owlCarousel({
+      loop: true,
+      margin: 20,
+      nav: true,
+      navText: [
+        "<i class='fa fa-angle-left'></i>",
+        "<i class='fa fa-angle-right'></i> ",
+      ],
+      dots: false,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 1,
+        },
+        1000: {
+          items: 4,
+        },
+      },
+    });
+
+    jQuery('.trending-content').each(function () {
+      var highestBox = 0;
+      jQuery('.tab-pane', this).each(function () {
+        if (jQuery(this).height() > highestBox) {
+          highestBox = jQuery(this).height();
+        }
+      });
+      jQuery('.tab-pane', this).height(highestBox);
+    });
+
+    if (jQuery('select').hasClass('season-select')) {
+      jQuery('select').select2({
+        theme: 'bootstrap4',
+        allowClear: false,
+        width: 'resolve',
+      });
+    }
   });
 })(jQuery);
